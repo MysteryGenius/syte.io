b79a0f7504f6f694604b66875c8944e537e06d7a
JayZ <jianzhen@doyobi.com>
2021-02-22 18:02:58 +0800

    feat: PWA & icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,15 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
+      name: 'syte.',
       lang: 'en',
+      theme_color:'#ffffff',
+      background_color:' #ffffff',
+      display: 'standalone',
+      icons: [
+        {"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},
+        {"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}
+      ]
     },
   },
 
